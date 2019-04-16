f63aa0caa4412c97beb4ce942b7e479699e23d13
lph <it_lph@126.com>
2019-04-16 11:57:20 +0800

    测试 - swagger测试

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,12 @@ public class TestController {
         log.info("*************test************");
         String str = "hello world";
         return "123";
+    }
+
+    @ApiOperation(value = "求一个数的5倍方法",notes = "输入一个整数")
+    @ApiImplicitParam(name = "num",value = "用户输入的参数",dataType = "int",paramType = "path")
+    @GetMapping("/getSum/{num}")
+    public int getSum(@PathVariable("num") int num){
+        return 5*num;
     }
 }
